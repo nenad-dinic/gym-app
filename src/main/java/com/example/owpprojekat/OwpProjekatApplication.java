78c9ae0c06b603c0514376ba2fcc9a59643decb3
Nenad Dinic <dinic.sr44.2020@uns.ac.rs>
2022-08-12 10:22:49 +0200

    inicijalni commit

---
+++ src/main/java/com/example/owpprojekat/OwpProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.example.owpprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwpProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwpProjekatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/owpprojekat/OwpProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.owpprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwpProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
