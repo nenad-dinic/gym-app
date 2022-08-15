26103f8f11c6f48ed5c29ab0a1a9cb6047fec644
Nenad Dinic <dinic.sr44.2020@uns.ac.rs>
2022-08-15 19:38:28 +0200

    manje izmene

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -1,5 +1,3 @@
-let lastSortedColumn = -1;
-
 function filterTrainings() {
     let name = document.getElementById("name").value;
     let type = document.getElementById("type").value;
@@ -41,29 +39,4 @@ function filterTrainings() {
 
     }
 
-}
-
-function sortTable(column) {
-    let direction = 0;
-    if (lastSortedColumn == column) {
-        direction = !direction;
-    }
-    lastSortedColumn = column;
-
-    let rows = document.getElementById("trainingTable").getElementsByTagName("tr");
-    let x;
-    let y;
-
-    for (let i = 1; i < rows.length - 1; i++) {
-        for (let j = i + 1; j < rows.length; j++) {
-            x = rows[i].getElementsByTagName("td")[column].innerText.toLowerCase();
-            y = rows[j].getElementsByTagName("td")[column].innerText.toLowerCase();
-
-            if (x < y && direction == 0) {
-                rows[i].parentNode.insertBefore(rows[j], rows[i]);
-            } else {
-                rows[i].parentNode.insertBefore(rows[j], rows[i]);
-            }
-        }
-    }
 }
